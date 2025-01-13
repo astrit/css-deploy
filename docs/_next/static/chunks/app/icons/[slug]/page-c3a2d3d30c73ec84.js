2de046003d187a7c4845676926ad3db2c722d00f
astrit <am@astrit.co>
2025-01-13 20:12:03 +0000

    Deploy

---
+++ docs/_next/static/chunks/app/icons/[slug]/page-c3a2d3d30c73ec84.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[790,454,409,626,931,318,744],{3521:function(){}},function(n){n.O(0,[971,23,560],function(){return n(n.s=3521)}),_N_E=n.O()}]);
